ff19cc368f1f0e176c5d4d447dbf9bf9f29e11f2
Thomas Sankara <thomassankara74@gmail.com>
2021-06-15 23:22:10 +0300

    section2 data

---
+++ src/data/section2.js
@@ -0,0 +1,16 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const section2 = [
+  {
+    id: uuidv4(),
+    image: "./images/image-graphic-design.jpg",
+    title: "Graphic Design",
+    desc: "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potenial clients' attention.",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/image-photography.jpg",
+    title: "Photography",
+    desc: "Increase your credibility by getting the most stunning, high-quality photos that improve your business image.",
+  },
+]
