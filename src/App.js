e41e6541d8b5c3345195e31204cab21d584ca006
Thomas Sankara <thomassankara74@gmail.com>
2021-06-15 23:22:50 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer"
+import Grid from "./components/Grid"
+import Header from "./components/Header"
+import Section1 from "./components/Section1"
+import Section2 from "./components/Section2"
+import Showcase from "./components/Showcase"
+import Testimonials from "./components/Testimonials"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Showcase />
+      <Section1 />
+      <Section2 />
+      <Testimonials />
+      <Grid />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
