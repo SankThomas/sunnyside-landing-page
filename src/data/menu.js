c0858180ae57ae6beb0ab80de730ec7d1ca5890e
Thomas Sankara <thomassankara74@gmail.com>
2021-06-15 23:21:52 +0300

    navbar data

---
+++ src/data/menu.js
@@ -0,0 +1,16 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const menu = [
+  {
+    id: uuidv4(),
+    title: "About",
+  },
+  {
+    id: uuidv4(),
+    title: "Services",
+  },
+  {
+    id: uuidv4(),
+    title: "Projects",
+  },
+]
