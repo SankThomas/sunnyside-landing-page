35393cb9a99da77ccd76f90aca9d1453bf3e5267
Thomas Sankara <thomassankara74@gmail.com>
2021-06-15 23:22:20 +0300

    testimonials data

---
+++ src/data/testimonials.js
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const testimonials = [
+  {
+    id: uuidv4(),
+    image: "./images/image-emily.jpg",
+    text: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+    name: "Emily R.",
+    title: "Marketing Director",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/image-thomas.jpg",
+    text: "Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience.",
+    name: "Thomas S.",
+    title: "Chief Operating Officer",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/image-jennie.jpg",
+    text: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+    name: "Jennie F.",
+    title: "Business Owner",
+  },
+]
