15af3421d46ce451059b348029fb48d106640cc9
Thomas Sankara <thomassankara74@gmail.com>
2021-06-15 23:21:59 +0300

    section1 data

---
+++ src/data/section1.js
@@ -0,0 +1,18 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const section1 = [
+  {
+    id: uuidv4(),
+    image: "./images/image-transform.jpg",
+    title: "Transform your brand",
+    desc: "We are a full service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.",
+    button: "Learn More",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/image-stand-out.jpg",
+    title: "Stand our to the right audience",
+    desc: "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.",
+    button: "Learn More",
+  },
+]
