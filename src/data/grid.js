abe2647761d525eed409d1a5ff096b94e81fd78a
Thomas Sankara <thomassankara74@gmail.com>
2021-06-15 23:21:45 +0300

    grid data

---
+++ src/data/grid.js
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const grid = [
+  {
+    id: uuidv4(),
+    image: "./images/image-gallery-milkbottles.jpg",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/image-gallery-orange.jpg",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/image-gallery-cone.jpg",
+  },
+  {
+    id: uuidv4(),
+    image: "./images/image-gallery-sugar-cubes.jpg",
+  },
+]
